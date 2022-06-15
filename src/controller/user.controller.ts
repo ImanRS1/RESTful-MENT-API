84819ed73f9b01703d0c960934193d4955a3e2fb
Iman Reza Soltani <iman.soltani@bonniernews.se>
2022-06-15 20:44:49 +0200

    session model created

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,14 +1,16 @@
 import { Request, Response } from 'express'
+import { omit } from 'lodash'
+import { CreateUserInput } from '../schema/user.schema'
 import { createUser } from '../service/user.service'
 import logger from '../utils/logger'
 
-export const createUserHandler = async (req: Request, res: Response) => {
+export const createUserHandler = async (req: Request<{}, {}, CreateUserInput["body"]>, res: Response) => {
   try {
     const user = await createUser(req.body)
-    return user
+    return res.send(omit(user.toJSON()), "password");
   } catch (error:any ) {
     logger.error(error)
-    res.status(409).send(error.message)
+    res.status(409).send(error.message);
   }
 
 }
