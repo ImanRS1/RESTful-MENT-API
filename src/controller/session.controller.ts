b519451004430c600da34216afb87146b8597ad5
Iman Reza Soltani <iman.soltani@bonniernews.se>
2022-06-20 11:36:03 +0200

    access and refresh tokens added

--- src/controller/session.controller.ts
+++ src/controller/session.controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express'
 import { validatePassword } from '../service/user.service'
 import { createSession } from '../service/session.service'
+import { signJwt } from '../utils/jwt.utils'
+import config from 'config'
 
 export const createUserSessionHandler = async (req: Request, res: Response) => {
   // Validate the users password
@@ -11,12 +13,22 @@ export const createUserSessionHandler = async (req: Request, res: Response) => {
   }
 
   // create a session
-  const session = createSession(user._id, req.get("user-agent") || "");
+  const session = await createSession(user._id, req.get("user-agent") || "");
 
   // create an access token
-  
+  const accessToken = signJwt(
+    {...user, 
+      session: session._id},
+    {expiresIn: config.get('accessTokenTtl')}
+  )
 
   // create a refresh token
+  const refreshToken = signJwt(
+    {...user, 
+      session: session._id},
+    {expiresIn: config.get('refreshTokenTtl')}
+  )
 
   // return access & refresh tokens
+  return res.send({accessToken, refreshToken})
 }
