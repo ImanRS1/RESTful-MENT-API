0e908414ddec72f8ebe44470af730e0c1cea4f62
Iman Reza Soltani <iman.soltani@bonniernews.se>
2022-07-17 22:28:17 +0200

    product controller added

---
+++ src/controller/product.controller.ts
@@ -0,0 +1,17 @@
+import {Request, Response} from 'express'
+
+export const createProductHandler = async (req: Request, res: Response) => {
+
+}
+
+export const updateProductHandler = async (req: Request, res: Response) => {
+
+}
+
+export const getProductHandler = async (req: Request, res: Response) => {
+
+}
+
+export const deleteProductHandler = async (req: Request, res: Response) => {
+
+}
